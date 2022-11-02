3e130adab1561d12331c3e324107c45662d7a481
Pol1nka <akulam75@gmail.com>
2022-11-02 21:25:40 +0500

    two commit

---
+++ laba5n2/laba5n2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Laboratorka5
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Введите число: ");
+
+            string number = Console.ReadLine();
+            bool flag = true;
+            int sum = 0;
+            int numCount = number.Length;
+            for (int i = 0; i < numCount; i++)
+            {
+                char element = (char)number[i];
+                if (!(element >= 48 && element <= 57))
+                {
+                    Console.WriteLine("Данное число не целое!");
+                    flag = false;
+                    break;
+                }
+                sum = sum + element - '0';
+            }
+
+            if (flag)
+            {
+                Console.WriteLine($"Сумма цифр данного числа: {sum}");
+            }
+        }
+    }
+}
